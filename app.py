d616a1b44873807af206d90fa4ecdfa50f7e9499
santiago_valenzuela <svalenzuela@nexura.com>
2026-01-29 15:11:46 -0500

    Correccion del app.py

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask, jsonify
-from src.routes.users import users_bp
 import os
+# Importamos el objeto strat_bp desde el archivo rutas
+from src.routes.users import strat_bp 
 
 app = Flask(__name__)
 
+# Ruta de salud para Cloud Run
 @app.route('/health')
 def health():
-    return jsonify({"status": "up", "db": "mysql"}), 200
+    return jsonify({
+        "status": "up", 
+        "service": "mod-conexionMySQL",
+        "database": "MySQL"
+    }), 200
 
-app.register_blueprint(users_bp, url_prefix='/api/v1/users')
+# Registro del Blueprint con el prefijo correcto
+# Esto hará que tus rutas sean: /api/v1/strat/
+app.register_blueprint(strat_bp, url_prefix='/api/v1/strat')
 
 if __name__ == '__main__':
+    # Cloud Run define la variable PORT, si no existe usa 8080
     port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
